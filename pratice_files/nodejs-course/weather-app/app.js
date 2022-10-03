e62ea55a3a381d09f5e8b67cbbac107835e3aec9
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2022-10-03 14:57:14 +0100

    response to log formated.

--- pratice_files/nodejs-course/weather-app/app.js
+++ pratice_files/nodejs-course/weather-app/app.js
@@ -2,15 +2,15 @@
 
 // Modules 
 import fetch from 'node-fetch';
-import api from './api-key.json' assert {type: 'json'};
+// import api from './api-key.json' assert {type: 'json'};
 
 const location = process.argv.slice(2);
 const makeRequest = async (location) => {
     try {
-        const response = await fetch(`https://yahoo-weather5.p.rapidapi.com/weather?location=${location}&format=json&u=c&rapidapi-key=${api.key}`);
+        const response = await fetch(`https://yahoo-weather5.p.rapidapi.com/weather?location=${location}&format=json&u=c&rapidapi-key=44ecf4522emsh48f75419e76afd8p193823jsn951ccde94696`);
         const data = await response.json();
-        const weather = data.current_observation.condition
-        console.log('Today\'s temperature: ', weather.temperature + 'celcius');
+        const weather = data.current_observation.condition;
+        console.log(`It is currently ${weather.temperature} degrees celcius out. Today will be ${weather.text}. `);
     } catch (e) {
         console.log('unable to connect to the server')
     }
